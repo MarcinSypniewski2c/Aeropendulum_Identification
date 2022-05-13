89906a5f6c370c1307ee89ca7ab8b21bb7208efb
Adrian Wójcik <54798218+adrianwojcikpp@users.noreply.github.com>
2022-05-13 12:54:22 +0200

    Initial commit

--- uartSteval.cpp
+++ uartSteval.cpp
@@ -1,19 +1,29 @@
 /**
- * @file uartSteval.cpp
- *
- *  Created on: Nov 27, 2020
- *      Author: Jakub Walkowski
- */
+  ******************************************************************************
+  * @file    uartSteval.h
+  * @author  Jakub Walkowski
+  * @author  AW					adrian.wojcik@put.poznan.pl
+  * @version 2.0
+  * @date    13-May-2022
+  * @brief   Serial port communication protocol for STEVAL-SPIN3021
+  *
+  ******************************************************************************
+  */
 
+/* Includes ------------------------------------------------------------------*/
 #include <ctype.h>
 #include <stdio.h>
-#include <wiringPi.h>
-#include <iostream>
-#include <unistd.h>
-#include <wiringSerial.h>
-#include "uartSteval.h"
 #include <stdarg.h>
 
+#include <iostream>
+#include <unistd.h>
+
+#include <wiringPi.h>
+#include <wiringSerial.h>
+
+#include "uartSteval.h"
+
+/* Defines -------------------------------------------------------------------*/
 #define DEBUG 0 //0
 
 #if DEBUG
@@ -23,7 +33,7 @@ void printf_debug(const char * format, ...)
   va_list args;
   va_start(args, format);
   vsprintf(buffer,format, args);
-  puts(buffer);
+  printf("%s",buffer);
   va_end(args);
 }
 #else
@@ -33,6 +43,7 @@ void printf_debug(const char * format, ...)
 }
 #endif
 
+/* Public methods ------------------------------------------------------------*/
 UART_STATUS receive(Frame *cmd,int connection)
 {
     int j=0;
@@ -54,28 +65,31 @@ UART_STATUS receive(Frame *cmd,int connection)
         printf_debug("ERROR\n");
     return UART_OK;
 }
-UART_STATUS sendData(int con, int data,int l, bool cmd_show=true)
+
+UART_STATUS sendData(int con, int data,int l)
 {
     for(int i = 0;i<l;i++){
       serialPutchar(con,(data >> i*8) & 0xFF);
-      if(cmd_show)
-        printf_debug("%02X ", (data >> i*8) & 0xFF); 
+      printf_debug("%02X ", (data >> i*8) & 0xFF); 
     }
-    if(cmd_show)
-        printf_debug("\n");
+    printf_debug("\n");
     return UART_OK;
 }
-UART_STATUS send(Frame cmd, UART uart,Frame* f, bool cmd_show)
+
+UART_STATUS send(Frame cmd, UART uart, Frame* f, bool cmd_show)
 {
+	/* THIS CANNOT BE DONE ON EVERY 'send' CALL!
     int connection = serialOpen("/dev/ttyACM0",uart.baud);
-    if(connection==-1)
+	
+    if(connection < 0)
     {
         return UART_CONNECTION_ERROR;
     }
+	*/
 
-    serialPutchar(connection,(cmd.motorId<<5)+(int)cmd.frameCode);
-    serialPutchar(connection,(int)cmd.payload);
-    serialPutchar(connection,(int)cmd.reg);
+    serialPutchar(uart.handler, (cmd.motorId<<5)+(int)cmd.frameCode);
+    serialPutchar(uart.handler, (int)cmd.payload);
+    serialPutchar(uart.handler, (int)cmd.reg);
 
     if (cmd_show)
     {
@@ -100,33 +114,42 @@ UART_STATUS send(Frame cmd, UART uart,Frame* f, bool cmd_show)
         printf_debug("\n");
     }
 
-    Frame recCommand= Frame();
+    Frame recCommand = Frame();
     //delay(3);
-    receive(&recCommand,connection);
+    receive(&recCommand, uart.handler);
+	
     *f = recCommand;
-    serialClose(connection);
+    
+	
+	//serialClose(connection);
+	
     return UART_OK;
 }
+
 UART_STATUS StartMotor(int motorId, UART uart,Frame* f)
 {
     Frame  cmd = Frame(1,FRAME_CODES::EXE,STEVAL_REGISTERS::START_MOTOR,(int)STEVAL_REGISTERS_LEN::EXE_CMD, NO_DATA);
     return send(cmd, uart,f);
 }
+
 UART_STATUS StopMotor(int motorId, UART uart,Frame* f)
 {
     Frame  cmd = Frame(1,FRAME_CODES::EXE,STEVAL_REGISTERS::STOP_MOTOR,(int)STEVAL_REGISTERS_LEN::EXE_CMD, NO_DATA);
     return send(cmd,uart,f);
 }
+
 UART_STATUS SetMotorRefSpeed(int ref, int motorId, UART uart,Frame* f)
 {
     Frame  cmd = Frame(1,FRAME_CODES::SET,STEVAL_REGISTERS::RAMP_FIN_SPEED,(int)STEVAL_REGISTERS_LEN::RAMP_FIN_SPEED, ref);
     return send(cmd,uart,f);
 }
+
 UART_STATUS SetRegistry(STEVAL_REGISTERS reg, STEVAL_REGISTERS_LEN regL, int motorId, UART uart, int data,Frame* f)
 {
     Frame cmd = Frame(1,FRAME_CODES::GET,reg,(int)regL,data);
     return send(cmd,uart,f);
 }
+
 UART_STATUS GetRegistry(STEVAL_REGISTERS reg, STEVAL_REGISTERS_LEN regL, int motorId, UART uart,Frame* f)
 {
     Frame cmd = Frame(1,FRAME_CODES::GET,reg,(int)regL,NO_DATA);
